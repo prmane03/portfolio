34b3709763cb5add09b5bbb6b22c14f03585a9da
pratik <prmane03@gmail.com>
2024-08-20 21:02:32 +0530

    Created common navbar and footer

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
-function collapseNav(){
+function collapseNav() {
     navb = document.getElementById("navBox");
-    if(navb.classList.contains('hidden')){
+    if (navb.classList.contains('hidden')) {
         navb.classList.add('block');
         navb.classList.remove('hidden');
-    }else{
+    } else {
         navb.classList.remove('block');
         navb.classList.add('hidden');
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPage = window.location.pathname.split('/').pop();
+    // Map of pages to nav link IDs
+    const navLinks = {
+        '': 'nav-home',
+        'about': 'nav-about',
+        'contact': 'nav-contact',
+        'portfolio': 'nav-portfolio' // Add more pages as needed
+    };
+
+    // Load the navigation bar
+    fetch('./components/nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('nav-id').innerHTML = data;
+            window.scrollTo(0, 0);
+            // Add 'active' class to the corresponding nav item
+            if (navLinks[currentPage]) {
+                document.getElementById(navLinks[currentPage]).classList.add('text-orange-600');
+            }
+        });
+
+    // Load the footer
+    fetch('./components/footer.html')
+        .then(response => response.text())
+        .then(data => document.getElementById('footer-id').innerHTML = data);
+
+    var footer = document.getElementById('footer-id');
+    if (document.body.scrollHeight <= window.innerHeight) {
+        footer.classList.add('fixed');
+        footer.classList.add('bottom-0');
+        footer.classList.add('inset-x-0');
+    }
+});
